10ba1910af5ea8ef4a5f988454d54a215f46cca1
Christian Berkhoff <chris@acid.cl>
2012-05-30 19:01:58 -0400

    cuasi funcionando

--- src/carioca/game.clj
+++ src/carioca/game.clj
@@ -1,5 +1,6 @@
 (ns carioca.game
-  (:use clojure.set))
+  (:use clojure.set
+        carioca.ui))
 
 (def suits
   [:spades :hearts :diamonds :clubs])
@@ -8,29 +9,187 @@
   (conj (range 2 11) :ace :jack :queen :king))
 
 (def deck
-  (conj (reduce concat
-           (map #(into [] (zipmap cards (repeat %)))
-                suits))
-        [:jocker :red]
-        [:jocker :black]))
+  (mapcat #(into [] (zipmap cards (repeat %))) suits))
 
+; implementar despues
 (defn new-game
   "Creates a hashmap with the state of a starting game. It
 requires the names of the players"
   [& players]
-  (let [n (count players)
+  (let [players (conj players :dealer)]
+    (let [n (count players)
         d (shuffle deck)]
-    (let [pd (partition 11 (rest d))]
-      {:draw_pile (apply concat (nthrest pd n))
-       :discard_pile (first d)
-       :players (zipmap players (take n pd))}))) 
+	    (let [pd (partition 2 d)]
+	      {:pile (apply concat (nthrest pd n))
+	       :players (zipmap players (take n pd))})))) 
 
-(defn winner?
-  "Returns the winner of the game or just nil"
+(def the-game (new-game "nigro" "imella")) 
+
+(:players the-game)
+(:pile the-game)
+
+(defn human-players
   [game]
-  (first (filter #(empty? (val %)) (:players game))))
+  (filter string? (keys (:players game))))
 
-(def tg (new-game "nigro" "imella")) 
 
-(winner? tg)
-(winner? (merge tg {:players {"trolus" ()}}))
+(human-players the-game)
+
+(defn player-hand
+  [game player-name]
+  ((:players game) player-name))
+
+(defn dealer-hand
+  [game]
+  (player-hand game :dealer))
+
+(player-hand the-game "imella")
+(dealer-hand the-game)
+
+(defn point-posibilities
+  [hand]
+  (letfn [(pp
+            [hand current branches]
+            (if (empty? hand)
+              branches
+              (let []
+                (if (number? current))
+              (let [next (first hand)
+                    r-hand (rest hand)
+                    next-branch (map #(+ % current) branches)]            
+                (if (= next :ace)
+                  (concat 
+                    (pp r-hand 1 next-branch)
+                    (pp r-hand 11 next-branch))
+                  (recur r-hand (if (number? next) next 10) next-branch))))))]
+         (pp hand (get (first hand) 0) [0])))
+
+(defn hand-points
+  [hand]
+  (let [v (map first hand)]
+    (let [n-aces (count (filter #(= :ace %) v))]
+      (let [easy-points (+ (reduce + (filter number? v))
+                           (* 10 (count (filter #(contains? #{:king :queen :jack} %) v))))]
+        (let [base (+ easy-points n-aces)]
+          (+ base (* 10 (min (quot (- 21 base) 10) n-aces))))))))        
+
+(hand-points [[:king] [2] [3] [4]]) ;19
+(hand-points [[:ace] [:ace] [:ace] [4]]) ;17
+(hand-points [[:ace] [:king] [3] [4]]) ;18
+(hand-points [[:ace] [4] [3] [4]]) ;12
+(hand-points [[:ace] [3] [3] [4]]) ;21
+(hand-points [[:ace] [:king] [:king] [:ace]]) ; 22
+
+(defn hand-to-set
+  [hand]
+  (set (map first hand)))
+
+(hand-to-set (player-hand the-game "imella"))
+
+(defn has-card
+  [hand cards]
+  (let [hand (hand-to-set hand)]
+    (not-empty (intersection cards hand))))
+
+(has-card (player-hand the-game "imella") #{8})
+
+(defn has-natural?
+  [hand]
+  (when (and (= (count hand) 2)
+           (has-card hand #{:ace})
+           (has-card hand #{:king :queen :jack 10}))
+    hand))
+
+(has-natural? [[:ace :spades] [:king :spades] [:queen :hearts]])
+(has-natural? [[:ace :spades] [7 :spades] [3 :hearts]])
+(has-natural? [[:ace :spades] [10 :spades]])
+
+(defn busted?
+  [hand]
+  (> (hand-points hand) 21))
+
+(defn hit
+  [game player-name]
+  (-> game
+    (update-in [:pile] rest)
+    (update-in [:players player-name] conj (first (:pile game)))))
+
+(hit the-game "imella")
+
+(player-dialog (dealer-hand the-game) 
+               (player-hand the-game "imella") 
+               "imella")
+
+(busted-dialog (dealer-hand the-game)
+               (player-hand the-game "imella") 
+               "imella")
+
+(busted-dialog (dealer-hand the-game)
+               (player-hand the-game "imella")
+               "imella"
+               false)
+
+(defn play-dealer-turn
+  [game]
+  (if (has-natural? (dealer-hand game))
+    (for [player-name (human-players game)]
+      (if (has-natural? (player-hand game player-name))
+        game
+        (do 
+          (busted-dialog (dealer-hand game)
+                       (player-hand game player-name)
+                       player-name
+                       false)
+          game)))
+    (if (< (hand-points (dealer-hand game)) 17)
+      (recur (hit game :dealer))
+      game
+      )))
+
+(play-dealer-turn the-game)
+
+(defn play-player-turn
+  [game player-name]
+  (loop [game game]
+    (if (= (player-dialog (dealer-hand game)
+                          (player-hand game player-name)
+                          player-name)
+           :hit)
+      (let [next-game (hit game player-name)]
+        (if (busted? (player-hand next-game player-name))
+           (do
+             (busted-dialog (dealer-hand game)
+                            (player-hand next-game player-name) 
+                            player-name)
+             next-game)
+          (recur next-game)))
+      game)))
+
+(play-player-turn the-game "imella")
+
+(defn play-players-turns
+  [game]
+  (reduce play-player-turn game (human-players game)))
+
+(play-players-turns the-game)
+
+(defn scores
+  [game]
+  (map #(if (> (second %) 21)
+          (conj % :busted)
+          %) 
+    (sort-by second 
+             (let [ps (:players game)]
+               (zipmap (keys ps) (map hand-points (vals ps)))))))
+
+(scores the-game)
+
+(defn play-game
+  [game]
+  (-> game
+    (play-players-turns)
+    (play-dealer-turn)
+    (scores)))
+
+(play-game the-game)
+(play-game (new-game "clojure-boy" "dr-lazy" "mr-eager"))
--- src/carioca/ui.clj
+++ src/carioca/ui.clj
@@ -1,28 +1,72 @@
 (ns carioca.ui
   (:use seesaw.core)
-  (:import (javax.swing JFrame JLabel JButton)))
+  (:import (javax.swing JFrame JLabel JButton)
+           (java.io File)))
 
 (native!)
 
-(defn owl
-  [c g]
-  (let [img (javax.imageio.ImageIO/read 
-      (java.net.URL. "http://web.me.com/chrisdiss3/SuffolkOwlSanctuary/files/european_owl_1_467x313.jpg"))]    
-    (draw g (.getGraphics img))))
+(defn card-to-file
+  [card]
+  (letfn [(n [i] (if (number? i) i (name i)))]
+     (str (n (get card 0)) "_" (n (get card 1)))))
 
+(defn card-icon
+  [card]
+  (icon (File. (str "resources" File/separator (card-to-file card) ".png"))))
 
-(def f 
-  (frame 
-    :title "Get to know Seesaw"
-    :content (button 
-               :icon (icon (java.net.URL. "http://web.me.com/chrisdiss3/SuffolkOwlSanctuary/files/european_owl_1_467x313.jpg")))))
+(defn card-label
+  [card]
+  (label :icon (card-icon card)))
 
-(-> f pack! show!)
+(defn card-button
+  [card]
+  (button :icon (card-icon card)))
 
-(def b (button :text "Click me!"))
-(alert "Shit")
-(input "Color?")
+(defn player-hand-items
+  [player-hand hide-first-card]
+  (let [card-labels (vec (map card-label player-hand))]
+    (interleave (if hide-first-card
+                  (cons (card-label [:back :red]) (rest card-labels))
+                  card-labels)
+                (repeat :separator))))
 
+(defn- bj-dialog
+  [title dealer-hand player-hand actions hide-first-dealer-card]
+  (-> (custom-dialog
+        :modal? true
+        :title title
+        :content (vertical-panel 
+                   :items [(horizontal-panel 
+                             :items (player-hand-items dealer-hand hide-first-dealer-card))
+                           (horizontal-panel 
+                             :items (player-hand-items player-hand false))
+                           (horizontal-panel
+                             :items actions)]))
+    pack!
+    show!))
+
+(defn busted-dialog
+  ""
+  ([dealer-hand player-hand player-name]
+    (busted-dialog dealer-hand player-hand player-name true))
+  
+  ([dealer-hand player-hand player-name hide-first-dealer-card]
+    (bj-dialog (str player-name " busted!")
+               dealer-hand
+               player-hand
+               [(action :name "Busted!" :handler #(return-from-dialog % :busted))]
+               hide-first-dealer-card)))
+
+(defn player-dialog
+  ""
+  [dealer-hand player-hand player-name]
+  (let [hit-act (action :name "Hit" :handler (fn [e] (return-from-dialog e :hit)))
+        stand-act (action :name "Stand" :handler (fn [e] (return-from-dialog e :stand)))]
+    (bj-dialog (str "Blackjack " player-name)
+               dealer-hand
+               player-hand
+               [hit-act stand-act]
+               true)))
 
 (defn create-crappy-ui
   []
